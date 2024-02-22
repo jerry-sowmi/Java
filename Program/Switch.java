286b8ddc0e2793b37c5fdfddc8b8f66a8d873bb5
jerry-sowmi <124442243+jerry-sowmi@users.noreply.github.com>
2024-02-22 09:03:24 +0000

    Add files via upload

---
+++ Program/Switch.java
@@ -0,0 +1,38 @@
+package s;
+
+public class Switch {
+	public static void main(String arg[]) {
+		int day = 4;
+		switch(day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");	
+			break;
+		case 3:
+			System.out.println("wednesday");	
+			break;
+		case 4:
+			System.out.println("Thursday");	
+			break;
+		case 5:
+			System.out.println("friday");	
+			break;
+		case 6:
+			System.out.println("saturday");	
+			break;
+			default:
+		case 7:
+			System.out.println("sunday");	
+			break;
+			
+			
+			
+			}
+		
+		
+	}
+	
+
+}
