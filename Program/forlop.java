8a3836a266d267926d6a32418181f8ecd52c988c
jerry-sowmi <124442243+jerry-sowmi@users.noreply.github.com>
2024-02-22 09:05:51 +0000

    Add files via upload

---
+++ Program/forlop.java
@@ -0,0 +1,12 @@
+package s;
+
+public class forlop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John";
+		System.out.println(name);
+
+	}
+
+}
