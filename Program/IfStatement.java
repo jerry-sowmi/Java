3f0a4115c4d470759a99820b2b89430da0124952
jerry-sowmi <124442243+jerry-sowmi@users.noreply.github.com>
2024-02-22 08:56:47 +0000

    Add files via upload

---
+++ Program/IfStatement.java
@@ -0,0 +1,18 @@
+package s;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int speed=70;
+		if(speed>70)
+		{
+			System.out.println("High Speed");
+		}
+		else {
+			System.out.println("Low Speed");
+		}
+
+	}
+
+}
