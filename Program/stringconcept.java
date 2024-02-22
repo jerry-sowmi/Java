d97f9b6220adb22dae43f8e8fdef00faca82acb5
jerry-sowmi <124442243+jerry-sowmi@users.noreply.github.com>
2024-02-22 09:19:33 +0000

    Add files via upload

---
+++ Program/stringconcept.java
@@ -0,0 +1,25 @@
+package s;
+
+public class stringconcept {
+	
+	public static void main(String []args) {
+		
+
+		        String s1 = "admin";
+		        String s2 = "admin";
+		        System.out.println("s1 value:" + s1);
+		        System.out.println("s2 value:" + s2);
+		        System.out.println("s1 length is:" + s1.length());
+		        System.out.println("s1 value charAt:" + s1.charAt(2));
+		        System.out.println("Equal of concept");
+		        if (s1.equals("admin") && s2.equals("admin")) {
+		            System.out.println("login success");
+		        } else {
+		            System.out.println("login fail");
+		        }
+		    }
+		
+
+	}
+
+
