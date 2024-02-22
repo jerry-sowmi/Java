672a19962210d781655133a36ed6a4c7068defd3
jerry-sowmi <124442243+jerry-sowmi@users.noreply.github.com>
2024-02-22 09:14:16 +0000

    Add files via upload

---
+++ Program/ODDEVEN.java
@@ -0,0 +1,15 @@
+package s;
+
+public class ODDEVEN {
+	public static void main(String arg[]) {
+		int num = 15;
+		if(num % 4  ==0) {
+			System.out.println(num + " is even");
+		
+	}
+		else  {
+			System.out.println(num + " is odd");
+	
+}
+	}
+}
